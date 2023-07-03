cd1d693febe527eda67d56dfa1bf14280c839f07
Siigmaf <faby.tbe@gmail.com>
2023-07-03 17:02:23 -0300

    test: Add tests for salesperson endpoint

---
+++ tests/integration/mocks.js
@@ -0,0 +1,29 @@
+const mockNewSalesPerson = {
+    "first_name": "Maria",
+    "last_name": "de Jesus",
+    "date_of_birth": "1975-05-03",
+    "email": "mjesus@example.com",
+    "password": "M123456"
+  }
+  
+  const mockSalesPersonList = [{
+    "id": "1",
+    "first_name": "Luke",
+    "last_name": "de Jesus",
+    "date_of_birth": "1975-05-03",
+    "email": "ljesus@example.com",
+    "password": "L123456"
+  },
+  {
+    "id": "2",
+    "first_name": "Dart",
+    "last_name": "da Silva",
+    "date_of_birth": "1972-02-06",
+    "email": "dsilva@example.com",
+    "password": "D123456"
+  }]
+
+  module.exports = {
+    mockNewSalesPerson,
+    mockSalesPersonList
+  }
---
+++ tests/integration/salesPerson.test.js
@@ -0,0 +1,35 @@
+const {mockNewSalesPerson, mockSalesPersonList} = require('./mocks')
+const chai = require('chai')
+const chaiHttp = require('chai-http')
+const sinon = require('sinon')
+
+const app = require('../../src/app')
+const connection = require('../../src/db/connection')
+
+const { expect, use } = chai
+
+use(chaiHttp)
+
+describe('SalesPerson endpoint tests', function () {
+  it("Should register a new salesperson", async function () {
+    sinon.stub(connection, 'execute').resolves([{ insertId: 50 }])
+    const response = await chai.request(app).post('/salesperson').send(mockNewSalesPerson)
+    const expectedResponse = {
+      id: 50,
+      first_name: 'Maria',
+      last_name: 'de Jesus',
+      date_of_birth: '1975-05-03',
+      email: 'mjesus@example.com'
+    }
+    expect(response.status).to.equal(201)
+    expect(response.body).to.deep.equal(expectedResponse)
+  })
+
+  it("Testing a salesperson's ", async function () {
+    sinon.stub(connection, 'execute').resolves([mockSalesPersonList])
+    const response = await chai.request(app).get('/salesperson')
+    expect(response.status).to.equal(200)
+    expect(response.body).to.deep.equal(mockSalesPersonList)
+  })
+  afterEach(sinon.restore)
+})
